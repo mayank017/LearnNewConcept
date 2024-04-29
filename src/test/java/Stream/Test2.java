6dd64c59c962416a36dcf159ec8ce12e89f87383
piglet <mayank.jaiswal17@gmail.com>
2024-04-29 19:02:59 +0530

    Update Test2.java

--- src/test/java/Stream/Test2.java
+++ src/test/java/Stream/Test2.java
@@ -4,6 +4,8 @@ public class Test2 {
     public static void main(String[] args) {
         int i = 1;
         int j = 10;
+        int k = 11;
+        ink l = 12;
         System.out.println(i + j);
     }
 }
