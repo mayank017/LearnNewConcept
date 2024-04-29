d3dfb157163cd8e88565f9fcc50e52ca342973ed
Mayank Jaiswal <Mayank.jaiswal17@gmail.com>
2024-04-29 19:04:26 +0530

    added a variable m

--- src/test/java/Stream/Test2.java
+++ src/test/java/Stream/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
     public static void main(String[] args) {
         int i = 1;
         int j = 10;
-        System.out.println(i + j);
+        int m = 12;
+        System.out.println(i + j + m);
     }
 }
